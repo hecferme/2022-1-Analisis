d9b0bd4c5eebc11d001f05ba5512e54d64833d20
HÉCTOR FERNÁNDEZ MÉNDEZ <FernandezMH@bccr.fi.cr>
2022-02-26 11:06:20 -0600

    dimensión de estratificación

--- script-dwadventureworkslt-objects.sql
+++ script-dwadventureworkslt-objects.sql
@@ -2,6 +2,9 @@
 use DWAdventureWorksLT
 
 
+if OBJECT_ID('DimEstratificacion', 'U') IS NOT NULL
+    drop table DimEstratificacion;
+
 if OBJECT_ID('FactSalesOrderDetail', 'U') IS NOT NULL
     drop table FactSalesOrderDetail;
 
@@ -29,6 +32,20 @@ IF TYPE_ID(N'Flag') IS NOT NULL
 CREATE TYPE [dbo].[Flag]  
 FROM bit NULL ;  
 
+create table DimEstratificacion (
+    ID int PRIMARY KEY, Class VARCHAR (30), LowerBound MONEY, UpperBound MONEY, Description VARCHAR (100));
+-- TODO !!!!!!! 
+-- Incluir los comandos insert para cargar los datos en la tabla
+BEGIN TRAN
+INSERT [dbo].[DimEstratificacion] ([ID], [Class], [LowerBound], [UpperBound], [Description]) VALUES (1000, N'PrecioArtículo', -999999999999.0000, 0.0000, N'Inválido')
+INSERT [dbo].[DimEstratificacion] ([ID], [Class], [LowerBound], [UpperBound], [Description]) VALUES (1100, N'PrecioArtículo', 0.0000, 1000.0000, N'De $0 menos de de $1000')
+INSERT [dbo].[DimEstratificacion] ([ID], [Class], [LowerBound], [UpperBound], [Description]) VALUES (1200, N'PrecioArtículo', 1000.0000, 2225.0000, N'De $1001 hasta menos de $2225')
+INSERT [dbo].[DimEstratificacion] ([ID], [Class], [LowerBound], [UpperBound], [Description]) VALUES (1300, N'PrecioArtículo', 2225.0000, 3197.0000, N'De $2225 hasta menos de $3197')
+INSERT [dbo].[DimEstratificacion] ([ID], [Class], [LowerBound], [UpperBound], [Description]) VALUES (1400, N'PrecioArtículo', 3197.0000, 999999999999.0000, N'De $3197 en adelante')
+INSERT [dbo].[DimEstratificacion] ([ID], [Class], [LowerBound], [UpperBound], [Description]) VALUES (3000, N'Salario', NULL, NULL, NULL)
+INSERT [dbo].[DimEstratificacion] ([ID], [Class], [LowerBound], [UpperBound], [Description]) VALUES (5000, N'Flete', NULL, NULL, NULL)
+INSERT [dbo].[DimEstratificacion] ([ID], [Class], [LowerBound], [UpperBound], [Description]) VALUES (10000, N'Tax', NULL, NULL, NULL)
+COMMIT;
 
 CREATE TABLE [DimAddress](
 	[AddressID] [int],
@@ -53,9 +70,17 @@ CREATE TABLE [DimProduct](
     [ProductModelName] [dbo].[Name],
     [SellStartDate] [datetime],
     [SellEndDate] [datetime],
-    [DiscontinuedDate] [datetime]
+    [DiscontinuedDate] [datetime],
+	[EstratificacionListPrice] [VARCHAR](100)
 )
 
+BEGIN TRAN
+UPDATE DimProduct set [EstratificacionListPrice] = 
+(select e.[Description] AS ListPriceEstratificado from DimProduct as p
+    left outer join DimEstratificacion e
+        on e.Class = 'PrecioArtículo' and e.LowerBound <= p.ListPrice and p.ListPrice < e.UpperBound
+		where p.ProductID = DimProduct.ProductID)
+COMMIT;
 GO
 CREATE TABLE [DimCustomer](
 	[CustomerID] [int],
